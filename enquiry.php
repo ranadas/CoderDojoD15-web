7011d90311d55b7e89d4263577c450d9988f6ead
Rana Das <rana.pratap.das@gmail.com>
2014-04-23 08:02:54 +0100

    Adding php form submit for enquiry form.

--- enquiry.php
+++ enquiry.php
@@ -20,30 +20,25 @@
         }
 
         $email_to="rana.pratap.das@gmail.com";
-        $email_subject="Question from CoderDojo Website";
+        $email_subject="Enquiry from CoderDojo Website";
 
-        $name = $_POST['name'];
-        $email_from = $_POST['email'];
-        $phone = $_POST['phone'];
+        $name = $_POST['enquiry_name'];
+        $email_from = $_POST['enquiry_email'];
+        $phone = $_POST['enquiry_phone'];
         $mentor = $_POST['mentor_student'] == "mentor" ? "mentor" : "student";
-        $topics = $_POST['topic'];
-        $topic_strings = implode(' ', $topics);
-//        echo($topic_strings);
-//          if(empty($aDoor)) {
-//            echo("You didn't select any topics.");
-//          }
-//          else {
-//            $N = count($aDoor);
-//
-//            echo("You selected $N topic(s): ");
+        if (isset($_POST['topic'])) {
+            $topics = $_POST['topic'];
+            $topic_strings = implode(' ', $topics);
+            //echo( count($topics)."</br>");
+            //echo("str : ".$topic_strings."</br>");
+//            $N=count($topics);
 //            for($i=0; $i < $N; $i++) {
-//              echo($aDoor[$i] . " ");
+//                echo($topics[$i] . " ===== </br>");
 //            }
-//          }
+        }
 
-
-        $message = $_POST['message'];
-        $company = $_POST['company'];
+        $message = $_POST['enquiry_message'];
+        $company = $_POST['enquiry_company'];
 
         // create email headers
         $headers = 'From: '.$name." <".$email_from.">\r\n".
@@ -51,30 +46,47 @@
         'X-Mailer: PHP/' . phpversion();
 
         //$sent = @mail($email_to, $email_subject, $message, $headers);
-        echo("<br />\n");
-        echo("name: " . $_POST['name'] . "<br />\n");
-        echo("email: " . $_POST['email'] . "<br />\n");
-        echo("message: " . $_POST['message'] . "<br />\n");
-        echo("mentor: " . $mentor . "<br />\n");
-        echo("company: " . $company . "<br />\n");
+//        echo("<br />\n");
+//        echo("name: " . $name . "<br />\n");
+//        echo("email: " . $email_from . "<br />\n");
+//        echo("message: " . $message. "<br />\n");
+//        echo("mentor: " . $mentor . "<br />\n");
+//        echo("company: " . $company . "<br />\n");
 
-        $message_body= "<b>".$name."</b></br>"." of type ".$mentor."</br>"." is interested <b>";
-        if(!empty($topic)) {
-            $message_body=$message_body.$topic_strings." </b>from </br>";
+//        $message_body= "<b>".$name."</b></br>"." of type ".$mentor."</br>"." is interested <b>";
+//        if(!empty($topic)) {
+//            $message_body=$message_body.$topic_strings." </b>from </br>";
+//        }
+
+//        $message_body=$message_body
+//        ." with company name  ".$company
+//        ."</br> with message </br>".$message;
+//
+        $email_message="<p> Message from : ".$name." (".$email_from.").</br>"
+        ."".$message
+        ."</br></br> Additional info :</br>"
+        ." Type : ".$mentor
+        ."</br>";
+        if(!empty($topics)) {
+            $email_message = $email_message."</br>"
+            ."with topic(s) ".$topic_strings;
         }
 
-        $message_body=$message_body
-        ." with company name  ".$company
-        ."</br> with message </br>".$message;
-        echo("message_body: " . $message_body . "<br />\n");
+        if(!empty($company)) {
+            $email_message = $email_message."</br>"
+            ."from company ".$company;
+        }
+
+        //echo("</br>email_message: " . $email_message . "<br />\n");
+        $sent = @mail($email_to, $email_subject, $message, $headers);
 //        echo("sent: " . $sent . "<br />\n");
 
-//        if ($sent) {
-//            header('Location: ' . $_SERVER['HTTP_REFERER']);
-//            exit();
-//        } else {
-//            echo("We encountered an error sending your mail");
-//        }
+        if ($sent) {
+            header('Location: ' . $_SERVER['HTTP_REFERER']);
+            exit();
+        } else {
+            echo("We encountered an error sending your mail");
+        }
     }
 
 ?>
